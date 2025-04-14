7443043eb90bd78d839f9b2530ca4f64c1db393f
Carolus <caroluspalin@icloud.com>
2025-04-14 16:09:22 +0300

    First task commit

---
+++ loops_conditions_exceptions.py.py
@@ -0,0 +1,8 @@
+# 1  For loop demonstration
+# This function prints numbers from 0 to 9 using a for loop
+
+def for_silmukka():
+    for i in range(10):
+        print(i, end=' ')
+        
+for_silmukka()

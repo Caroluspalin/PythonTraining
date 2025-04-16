5e54ca8c9b3254bba67fff4525ef1bff5543c631
Carolus <caroluspalin@icloud.com>
2025-04-16 19:26:09 +0300

    Second task added

--- functions_and_arguments.py
+++ functions_and_arguments.py
@@ -13,4 +13,18 @@ eka_funktio()
 
 
 def main():
-    eka_funktio 
+    print_powers
+
+#2 Print a rectangle of asterisks with user-defined height and width
+
+def print_rectangle(korkeus, leveys):
+    for _ in range(korkeus):
+        print("*" * leveys)
+
+def main():
+    korkeus = int(input("Anna suorakulmion korkeus: "))
+    leveys = int(input("Anna suorakulmion leveys: "))
+    print_rectangle(korkeus, leveys)
+
+if __name__ == "__main__":
+    main()

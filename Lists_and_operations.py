fc67e4dd7ff20538f0feed460db462781dbb59ba
Carolus <caroluspalin@icloud.com>
2025-04-25 18:15:45 +0300

    New task

--- Lists_and_operations.py
+++ Lists_and_operations.py
@@ -4,3 +4,17 @@ def tulosta_lista():
     print(*lista)
 
 tulosta_lista()
+
+#2 Collect words until 'x' is entered, then print each word on a new line starting with 'x'
+def sanat_jarjestykseen():
+    lista = []
+    while True:
+        sana = input("Anna sana (x lopettaa): ")
+        if sana == "x":
+            break
+        lista.append(sana)
+    lista.sort()
+    for sana in lista:
+        print("x" + sana)
+
+sanat_jarjestykseen()

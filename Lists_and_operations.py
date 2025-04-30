25b36f6a0a7932b888b8565479ec87ecfbd67b78
Carolus <caroluspalin@icloud.com>
2025-04-30 16:45:04 +0300

    new task

--- Lists_and_operations.py
+++ Lists_and_operations.py
@@ -164,3 +164,27 @@ def yksilolliset_lista():
     print(', '.join(map(str, kaikki)))
 
 yksilolliset_lista()
+
+
+
+#12 Check if a given row in a Sudoku grid contains only unique non-zero values
+
+def rivi_oikein(sudoku: list, rivi_nro: int) -> bool:
+    rivi = sudoku[rivi_nro]
+    luvut = [x for x in rivi if x != 0]
+    return len(luvut) == len(set(luvut))
+
+sudoku = [
+    [9, 0, 0, 0, 8, 0, 3, 0, 0],
+    [2, 0, 0, 2, 5, 0, 7, 0, 0],
+    [0, 2, 0, 3, 0, 0, 0, 0, 4],
+    [2, 9, 4, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 7, 3, 0, 5, 6, 0],
+    [7, 0, 5, 0, 6, 0, 4, 0, 0],
+    [0, 0, 7, 8, 0, 3, 9, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 3],
+    [3, 0, 0, 0, 0, 0, 0, 0, 2]
+]
+
+print(rivi_oikein(sudoku, 0))
+print(rivi_oikein(sudoku, 1))

69e863263487cf78cc798a01690850c791ab997a
Carolus <caroluspalin@icloud.com>
2025-05-05 23:46:50 +0300

    new task

--- strings_string_manipulation.py
+++ strings_string_manipulation.py
@@ -9,3 +9,27 @@ def merkkijonot_1():
 
 
 merkkijonot_1()
+
+
+#2 count how many times user inputs helmi break program when user writes quit
+def helminauha():
+    count = 0
+    first_input = True
+
+    while True:
+        if first_input:
+            text = input("Anna ensimmäinen teksti: ")
+            first_input = False
+        else:
+            text = input("Anna seuraava teksti: ")
+
+        if text == "quit":
+            break
+        if text.lower() == "helmi":
+            count += 1
+
+    if count > 0:
+        print(f"{count} helmeä")
+    print("Näkemiin!")
+
+helminauha()

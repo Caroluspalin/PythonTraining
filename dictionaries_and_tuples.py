45cfac5908ae3cdd14336d3dcc50f9656c97c058
Carolus <caroluspalin@icloud.com>
2025-05-07 22:51:01 +0300

    new task

--- dictionaries_and_tuples.py
+++ dictionaries_and_tuples.py
@@ -9,3 +9,24 @@ def sanakirja_team_1():
     print(team)
 
 sanakirja_team_1()
+
+
+# 2. Print team members in original and sorted order.
+
+def sanakirja_team_2():
+    team = {
+        "Jukka": "software developer",
+        "Anne": "project manager",
+        "Susanna": "software developer",
+        "Aliisa": "lead developer"
+    }
+
+    for name in team:
+        print(name, end=' ')
+    print()
+
+    for name in sorted(team):
+        print(name, end=' ')
+    print()
+
+sanakirja_team_2()

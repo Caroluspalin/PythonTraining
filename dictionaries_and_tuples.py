d18f139cd487da69b719287600bbfdc231bfc2b1
Carolus <caroluspalin@icloud.com>
2025-05-07 22:43:31 +0300

    new subject

---
+++ dictionaries_and_tuples.py
@@ -0,0 +1,11 @@
+# 1. Create and print a team role dictionary.
+def sanakirja_team_1():
+    team = {
+        'Jukka': 'software developer',
+        'Anne': 'project manager',
+        'Susanna': 'software developer',
+        'Aliisa': 'lead developer'
+    }
+    print(team)
+
+sanakirja_team_1()
